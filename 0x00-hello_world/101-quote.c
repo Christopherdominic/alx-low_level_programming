171b62488a12473b920e8ef09a5ecd62b670981e
Christopher-Dominic <chriseze0@gmail.com>
2023-07-13 19:17:19 -0700

    last tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+#include <unistd.h>
+/**
+ * main - Prints to the standard error using write.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
